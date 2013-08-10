5ce06f5bc194d9b3df43620d61dfcfaf88235f73
Andrew Dutcher <andrew@andrewdutcher.com>
2013-08-09 18:27:47 -0700

    Added dummy settings activity

--- src/com/andrewdutcher/indexcards/MainSettings.java
+++ src/com/andrewdutcher/indexcards/MainSettings.java
@@ -1,32 +1,12 @@
 package com.andrewdutcher.indexcards;
 
-import android.app.Activity;
+import android.preference.PreferenceActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainSettings extends Activity {
+public class MainSettings extends PreferenceActivity {
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		
-		super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        
-	}
-	
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.activity_settings, menu);
-        return false;
-    }
-	
-	@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-	
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-    	super.onWindowFocusChanged(hasFocus);
+	public void onCreate(Bundle saved) {
+		super.onCreate(saved);
+		addPreferencesFromResource(R.layout.activity_settings);
 	}
 }
